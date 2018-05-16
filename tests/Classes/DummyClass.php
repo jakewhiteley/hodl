8fd5d5c16d5d9d55d508e720882245802d1e1ae1
Jake Whiteley <jake.whiteley@toolstation.com>
2018-05-16 17:15:45 +0100

    Added autowiring for methods

--- tests/Classes/DummyClass.php
+++ tests/Classes/DummyClass.php
@@ -8,9 +8,35 @@ class DummyClass
 
     public $bar = null;
 
+
     public function __construct($string = 'not_set')
     {
         $this->foo = $string;
         $this->bar = random_bytes(5);
+    }
+
+    public function hasNoStaticParams(Resolver $resolver)
+    {
+        return $resolver;
+    }
+
+    public static function isStatic(Resolver $resolver)
+    {
+        return $resolver;
+    }
+
+    public static function staticHasNoParams()
+    {
+        return true;
+    }
+
+    public function hasNoParams()
+    {
+        return true;
+    }
+
+    public function hasParams(Resolver $resolver, $param = null)
+    {
+        return $param;
     }
 }
--- tests/Classes/NeedsResolving.php
+++ tests/Classes/NeedsResolving.php
@@ -6,8 +6,17 @@ class NeedsResolving
 {
     public $resolver = null;
 
+    public $methodResolver = null;
+
     public function __construct(Resolver $resolver)
     {
         $this->resolver = $resolver;
+    }
+
+    public function hasNoStaticParams(Resolver $resolver)
+    {
+        $this->methodResolver = $resolver;
+
+        return $this->methodResolver;
     }
 }
